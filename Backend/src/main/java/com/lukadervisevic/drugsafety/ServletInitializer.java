b9e44c0bd6daa9157072bf76438f7dca57e72228
LukaDervisevic <lukadervisevic@gmail.com>
2025-10-12 22:12:12 +0200

    adding frontent file structure

--- Backend/src/main/java/com/lukadervisevic/drugsafety/ServletInitializer.java
+++ Backend/src/main/java/com/lukadervisevic/drugsafety/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DrugsafetyApplication.class);
+		return application.sources(BackendApplication.class);
 	}
 
 }
--- Backend/src/main/java/com/lukadervisevic/drugsafety/model/Lek.java
+++ Backend/src/main/java/com/lukadervisevic/drugsafety/model/Lek.java
@@ -4,8 +4,14 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Lek {
     @Id
     private int id;
