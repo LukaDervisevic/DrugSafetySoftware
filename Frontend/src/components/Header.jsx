981bb9e4dc4ec7ddd4c64ff9620ad437452078f4
LukaDervisevic <lukadervisevic@gmail.com>
2025-10-13 18:15:02 +0200

    commiting to switch to desktop

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,9 +1,18 @@
 
 function Header() {
     return <>
-        <div className="w-[100%] h-[200px]">
-            <div className="w-[100%] flex flex-row justify-between">
-                <div>Alimns logo</div>
+        <div className="w-[100%] h-[200px] flex justify-center items-center">
+            <div className="w-[100%] flex flex-row justify-between items-center">
+                <div className="ml-[30px] flex justify-center items-center">
+                    <a href="https://www.alims.gov.rs/" className="flex items-center no-underline ">
+                    <img src="/images/alims.png" alt="alims-logo" />
+                    <div className="ml-[20px] flex">
+                        <p className="text-2xl">alims</p>
+                        <p className="text-base">Agencija za lekove i medicinska sredstva Srbije</p>
+                    </div>
+                    
+                    </a>
+                </div>
                 <div className="flex flex-row">
                     <div>
                         <input type="text" />
